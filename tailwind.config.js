4201fdf71ee03d7e6ba2335924db1c0f835f5354
valetinedesign <dk7414857161@gmail.com>
2025-03-30 12:42:37 +0530

    responsive capibilities overview

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
         montserrat: ["Montserrat", "sans-serif"],
       },
     },
+    screens: {
+      'xs': '400px',
+      'sm': '600px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': '1920px',
+      '4k': '2560px',
+    },
   },
-  plugins: [],
 };
